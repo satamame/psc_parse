85e5ca4b5ef26c3abe94ce61fa49bfa1a6b260b1
satamame <satax@nifty.com>
2020-09-15 19:18:09 +0900

    First commit

---
+++ setup.py
@@ -0,0 +1,18 @@
+import setuptools
+ 
+setuptools.setup(
+    name="psc_parse",
+    version="0.1",
+    author="satamame",
+    author_email="satax@nifty.com",
+    description="Train/use tree model to parse play script",
+    long_description="Train/use tree model to parse play script",
+    long_description_content_type="text/markdown",
+    url="https://github.com/satamame/psc_parse",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ]
+)
