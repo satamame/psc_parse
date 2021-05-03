c2cbd2472a0f037b772c0a6359fc32931f3cfe00
satamame <satax@nifty.com>
2021-05-03 16:38:02 +0900

    fix_pep8

--- src/psc_parse/__init__.py
+++ src/psc_parse/__init__.py
@@ -4,3 +4,15 @@ from .mrph_test import mrph_test_dir
 from .mrph_match import MrphMatch, MRPH_MTCH_PTN
 from .features import make_features, features_in_lines
 from .model import get_dataset, make_model
+
+__all__ = [
+    'PscClass',
+    'JumanPsc',
+    'mrph_test_dir',
+    'MrphMatch',
+    'MRPH_MTCH_PTN',
+    'make_features',
+    'features_in_lines',
+    'get_dataset',
+    'make_model',
+]
---
+++ tests/juman_settings.py
@@ -0,0 +1,2 @@
+JUMAN_COMMAND = 'jumanpp_v2'
+JUMAN_OPTION = r'--config=C:\ProgramData\jumanpp\model\jumandic.conf'
