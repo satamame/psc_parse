65e4c0e5cd2a76ae84c93f56ae6765d5d1235622
satamame <satax@nifty.com>
2021-05-03 17:12:14 +0900

    fix pep8 warnings.

--- src/psc_parse/model.py
+++ src/psc_parse/model.py
@@ -10,14 +10,14 @@ from .features import ft_keys
 
 def get_dataset(targets_dir, features_dir):
     '''教師ラベルと、対応する特徴量データを取得する
-    
+
     Parameters
     ----------
     targets_dir : str
         教師ラベルファイルのディレクトリ
     features_dir : str
         特徴量ファイルのディレクトリ
-    
+
     Returns
     -------
     targets : list
@@ -25,49 +25,50 @@ def get_dataset(targets_dir, features_dir):
     features : list
         特徴量 (list) のリスト
     '''
-    
+
     targets = []
     features = []
-    
+
     for entry in os.scandir(path=targets_dir):
         if not entry.is_file():
             continue
-        
+
         # 特徴量データのファイル名
         basename = os.path.basename(entry).split('.', 1)[0]
         features_fname = os.path.join(features_dir, basename + '.csv')
-        
+
         # 特徴量データがなければ、その教師ラベルはスキップ
         if not os.path.isfile(features_fname):
             continue
-        
+
         # 教師ラベルファイルからラベルを取得
         with open(entry, encoding='utf_8_sig') as f:
             # 各行の空白文字以降を切り捨てた文字列を取得
             lines = f.readlines()
-        labels = [re.split(r'\s', l, 1)[0] for l in lines]
-        
+        labels = [re.split(r'\s', line, 1)[0] for line in lines]
+
         # 特徴量ファイルから対応する特徴量を取得
         with open(features_fname, encoding='utf_8_sig') as f:
-            lines = [l.rstrip() for l in f.readlines()]
-        fts = [l.split(',') for l in lines]
-        
+            lines = [line.rstrip() for line in f.readlines()]
+        fts = [line.split(',') for line in lines]
+
         # 少ない方の数に合わせる (エラーでも良いが)
         count = min(len(labels), len(fts))
         targets.extend(labels[:count])
         features.extend(fts[:count])
-    
+
     # 教師ラベルを数値に変換 (未定義ならコメントとする)
-    targets = [PscClass[t].value if t in PscClass._member_names_
+    targets = [
+        PscClass[t].value if t in PscClass._member_names_
         else PscClass.COMMENT.value
         for t in targets]
-    
+
     return targets, features
 
 
 def make_model(targets, features, max_depth):
     '''教師ラベルと、対応する特徴量データから決定木モデルを作成する
-    
+
     Parameters
     ----------
     targets : list
@@ -76,23 +77,23 @@ def make_model(targets, features, max_depth):
         特徴量 (list) のリスト
     max_depth : int
         決定木の最大深度
-    
+
     Returns
     -------
     tree : DecisionTreeClassifier
         決定木モデル
     '''
-    
+
     # 決定木を学習させる
     tree = DecisionTreeClassifier(max_depth=max_depth)
     tree.fit(features, targets)
-    
+
     return tree
 
 
 def predict(juman, tree, lines, normalize=False):
     '''決定木モデルを使って台本の各行の種類を予測する
-    
+
     Parameters
     ----------
     juman: JumanPsc
@@ -103,13 +104,13 @@ def predict(juman, tree, lines, normalize=False):
         行 (str) のリスト
     normalize : bool
         正規化するかどうか
-    
+
     Yields
     ------
     label : PscClass
         各行の種類
     '''
-    
+
     # 登場人物見出しが出た後か
     charsheadline_used = 0
     # 柱 (レベル1) が出た後か
@@ -146,7 +147,7 @@ def predict(juman, tree, lines, normalize=False):
         labels = tree.predict([vals])
         label = labels[0]
         yield label
-        
+
         # 次ループ以降のための特徴量の更新
         if label == PscClass.CHARSHEADLINE.value:
             charsheadline_used = 1
@@ -161,7 +162,7 @@ def predict(juman, tree, lines, normalize=False):
 
 def give_labels(juman, tree, in_file, out_file, normalize=False):
     '''台本ファイルに予測したラベルをつけて保存する
-    
+
     Parameters
     ----------
     juman: JumanPsc
@@ -175,15 +176,15 @@ def give_labels(juman, tree, in_file, out_file, normalize=False):
     normalize : bool
         正規化するかどうか
     '''
-    
+
     # 入力ファイルから行を取り出す
     with open(in_file, encoding='utf_8_sig') as in_f:
-        lines = [l for l in in_f.readlines()]
+        lines = [line for line in in_f.readlines()]
 
     # 各行から行の種類を予測して文字列のジェネレータにする
     classes = predict(juman, tree, lines, normalize=normalize)
     labels = (PscClass(c).name for c in classes)
-        
+
     # 出力ファイルにラベル付きの行を書き出す
     with open(out_file, 'w', encoding='utf_8_sig') as out_f:
         for line, label in zip(lines, labels):
